b0b95b133c61bec4a495ae1000d495a1280bd287
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-12-03 07:20:07 +0100

    finish

--- ToDo.js
+++ ToDo.js
@@ -1,64 +1,98 @@
+(function (){
 class ThingToDo {
-
-    constructor() {
-        this.exercises = []
+    constructor(appendLocation) {
+        this.tasks = JSON.parse(localStorage.getItem('thingToDoList')) || []
+        this.appendLocation = appendLocation || document.body
         this.render()
     }
-    addExercise(text) {
-        this.exercises.push(new Exercise(text))
+    addTask(text) {
+        this.tasks.push(new Task(text))
+        this.addTasksToLocalStorage()
         this.render()
 
     }
+    deleteTask(index) {
+        this.tasks = this.tasks.slice(0, index).concat(this.tasks.slice(index + 1))
+        this.render()
+    }
+
+    toggleTask(task) {
+        if (task.isCompleted === true) {
+            task.isCompleted = false
+        } else {
+            task.isCompleted = true
+        }
+        this.render()
+    }
+
+    taskIsCompletedStyle(task, li) {
+        if (task.isCompleted === false) {
+            li.style.textDecoration = "none"
+        } else {
+            li.style.textDecoration = "line-through"
+        }
+    }
+
+    addTasksToLocalStorage() {
+        localStorage.setItem('ThingToDoList', `${JSON.stringify(this.tasks)}`)
+
+    }
+
     render() {
-        document.body.innerHTML = ''
+        this.appendLocation.innerHTML = ''
 
         this.makePort()
 
         const ul = document.createElement('ul')
 
-        this.exercises.forEach((exercise, index) => {
+        this.tasks.forEach((task, index) => {
             const li = document.createElement('li')
             const buttonDelete = document.createElement('button')
-            buttonDelete.innerText = 'Delete exercise from the list'
+            buttonDelete.innerText = 'Delete task from list'
 
-            buttonDelete.addEventListener('click',
-            () => this.deleteExercise(index))
-            li.innerText = exercise.text
+            buttonDelete.addEventListener('click', () => this.deleteTask(index))
+
+            li.addEventListener('click', () => this.toggleTask(task))
+
+            this.taskIsCompletedStyle(task, li)
+
+            li.innerText = task.text
             li.appendChild(buttonDelete)
             ul.appendChild(li)
         })
-        document.body.appendChild(ul)
+        this.appendLocation.appendChild(ul)
 
     }
     makePort() {
         const input = document.createElement('input')
         const buttonAdd = document.createElement('button')
-        buttonAdd.innerText = 'Add exercise to list'
+        buttonAdd.innerText = 'Add task to list'
 
 
         buttonAdd.addEventListener(
             'click',
-            () => this.addExercise(input.value)
+            () => this.addTask(input.value)
         )
 
 
 
-        document.body.appendChild(input)
-        document.body.appendChild(buttonAdd)
+        this.appendLocation.appendChild(input)
+        this.appendLocation.appendChild(buttonAdd)
 
     }
 
-    deleteExercise (index) {
-        this.exercises = this.exercises.slice(0, index).concat(this.exercises.slice(index + 1))
-        this.render()
-    }
-    
+
 }
 
 
-class Exercise {
+class Task {
     constructor(text) {
         this.text = text
+        this.isCompleted = false
     }
 }
-const toDo1 = new ThingToDo()
+
+const newThingToTo1 = new ThingToDo()
+
+
+})()
